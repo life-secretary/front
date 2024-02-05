35a63eeb6d9481ffbb8d2dbe200bb6c11b25ab2e
ciocio97 <tmddusleo@icloud.com>
2024-02-05 16:46:00 +0900

    feat: react-native initial setting

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import { LogBox } from 'react-native';
+
+// add to ignore warning "RCTBridge required dispatch_sync to load REAModule"
+LogBox.ignoreLogs(['RCTBridge required dispatch_sync to load REAModule']);
+
+// start App
+AppRegistry.registerComponent(appName, () => App);
