06eaf74256e2d4e693afc822a5854724622c96e1
devsoladev <sorry0194@gmail.com>
2024-02-13 15:56:31 +0900

    feat: create a recoil store

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { LogBox } from 'react-native';
+import {LogBox} from 'react-native';
 
 // add to ignore warning "RCTBridge required dispatch_sync to load REAModule"
 LogBox.ignoreLogs(['RCTBridge required dispatch_sync to load REAModule']);
