9050d00f2f24c753f081d13ce974bb75fe57bc20
devsoladev <sorry0194@gmail.com>
2024-02-13 16:55:59 +0900

    feat: set up react navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import {RecoilRoot} from 'recoil';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {HomeScreen} from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <RecoilRoot>
-      <></>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </RecoilRoot>
   );
 }
