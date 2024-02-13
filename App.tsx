ff7956555df14974566a18cd698653c1695fa3b7
devsoladev <sorry0194@gmail.com>
2024-02-13 17:17:18 +0900

    feat: add a tab navigation

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import {RecoilRoot} from 'recoil';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {HomeScreen} from './src/screens/HomeScreen';
+import {TodoScreen} from './src/screens/TodoScreen';
+import {SaveScreen} from './src/screens/SaveScreen';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
   return (
     <RecoilRoot>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
+        <Tab.Navigator screenOptions={{headerShown: false}}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Todo" component={TodoScreen} />
+          <Tab.Screen name="Save" component={SaveScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </RecoilRoot>
   );
