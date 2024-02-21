6740c2d5cca2b5e7545c68bae9fb6c688c69f582
Sol Ahn <sorry0194@gmail.com>
2024-02-22 02:01:25 +0900

    feat: add the blank page in the save screen

--- src/components/save/SaveContentsList.tsx
+++ src/components/save/SaveContentsList.tsx
@@ -1,7 +1,23 @@
 import * as React from 'react';
 import {StyleSheet, View, FlatList} from 'react-native';
 import {SaveContentsItem} from './SaveContentsItem';
+import {AppText} from '../common/AppText';
 
+const EmptyList = () => {
+  return (
+    <View style={styles.emptyListContainer}>
+      <View style={styles.textContainer}>
+        <AppText style={styles.text}>저장된 콘텐츠가 없어요</AppText>
+        <AppText style={styles.subText}>
+          찾는 콘텐츠가 없다면 의견을 보내주세요
+        </AppText>
+      </View>
+      <View style={styles.buttonContainer}>
+        <AppText style={styles.button}>문의 및 의견 보내기</AppText>
+      </View>
+    </View>
+  );
+};
 export function SaveContentsList({list, isEditMode}: any): React.JSX.Element {
   return (
     <View style={styles.container}>
@@ -15,6 +31,7 @@ export function SaveContentsList({list, isEditMode}: any): React.JSX.Element {
           />
         )}
         ItemSeparatorComponent={() => <View style={styles.divider} />}
+        ListEmptyComponent={<EmptyList />}
         contentContainerStyle={styles.listContainer}
       />
     </View>
@@ -34,5 +51,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#F2F4F7',
     height: 1,
     marginVertical: 16,
+  },
+  emptyListContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 28,
+  },
+  textContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#000E24',
+  },
+  subText: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#526070',
+  },
+  buttonContainer: {
+    borderWidth: 1,
+    borderRadius: 6,
+  },
+  button: {
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    fontSize: 13,
+    fontWeight: '700',
   },
 });
