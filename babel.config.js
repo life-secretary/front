de226dc0de82cbc6e25771bc2c89846bf0e89663
Sol Ahn <sorry0194@gmail.com>
2024-02-28 22:08:13 +0900

    feat: set up the root path alias

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
