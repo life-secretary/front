e6deb8ccd76008e4c0f8072c40876b0e40a091ea
ciocio97 <tmddusleo@icloud.com>
2024-03-06 15:41:17 +0900

    feat: remove vector-icon module in ContentModal

--- src/components/common/AppIcon.tsx
+++ src/components/common/AppIcon.tsx
@@ -39,7 +39,7 @@ const AppIcon = ({
     
     const IconSvg = Icon[name];
 
-    console.log(styles);
+    // console.log(styles);
 
     // 아이콘 press 시 색상 변경값 통일 (확정 x)
     const IconSvgProps = {
