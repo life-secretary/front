e947bea07bdd06aeb59e65ccd19c3057b1af1a75
Sol Ahn <sorry0194@gmail.com>
2024-03-06 23:32:18 +0900

    feat: set up the carousel component

--- src/components/home/HomeImageCarousel.tsx
+++ src/components/home/HomeImageCarousel.tsx
@@ -1,18 +1,43 @@
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Dimensions, StyleSheet, View} from 'react-native';
 import {AppText} from '../common/AppText';
+import Carousel from 'react-native-reanimated-carousel';
 
 export function HomeImageCarousel(): React.JSX.Element {
+  const HORRIZONTAL_PADDING = 24 * 2;
+  const width = Dimensions.get('window').width - HORRIZONTAL_PADDING;
+
   return (
     <View style={styles.container}>
-      <AppText>Home Image Carousel</AppText>
+      <Carousel
+        loop
+        width={width}
+        height={450}
+        autoPlay={true}
+        data={[...new Array(4).keys()]}
+        scrollAnimationDuration={2000}
+        panGestureHandlerProps={{
+          activeOffsetX: [-10, 10],
+        }}
+        renderItem={({index}) => (
+          <View
+            style={{
+              flex: 1,
+              borderWidth: 1,
+              justifyContent: 'center',
+            }}>
+            <AppText style={{textAlign: 'center', fontSize: 30}}>
+              {index}
+            </AppText>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 450,
     borderWidth: 1,
     marginVertical: 10,
     justifyContent: 'center',
