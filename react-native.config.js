4ed09eed7cb985bbbc6c9cab34f3bbf4a9c8b011
ciocio97 <tmddusleo@icloud.com>
2024-03-12 21:27:44 +0900

    feat: resolve build error

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   assets: ['./src/assets/fonts/'],
   // To avoid add all fonts to the Build Phases, Copy Pod Resources
-  dependencies: {
-    'react-native-vector-icons': {
-      platforms: {
-        ios: null,
-      },
-    },
-  },
 };
