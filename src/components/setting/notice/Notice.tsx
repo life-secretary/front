be6575507e64ba0cbf940930e753444b0b4a1e72
Sol Ahn <sorry0194@gmail.com>
2024-05-11 10:43:34 +0900

    feat: read notice list by calling get api

--- src/components/setting/notice/Notice.tsx
+++ src/components/setting/notice/Notice.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {fetchData} from '@/api/api';
 
 import {ScrollView, StyleSheet, View} from 'react-native';
 import AppIcon from '@/components/common/AppIcon';
@@ -7,6 +8,8 @@ import Accordion from 'react-native-collapsible/Accordion';
 import color from '@/styles/color';
 import {font} from '@/styles/font';
 import spacing from '@/styles/spacing';
+
+import {getFormattedDate} from '@/utils';
 
 const EmptyList = () => (
   <View style={styles.emptyListContainer}>
@@ -18,29 +21,30 @@ const EmptyList = () => (
 );
 
 export function Notice(): React.JSX.Element {
+  const [noticeList, setNoticeList] = useState([]);
   const [activeNotices, setActiveNotices] = useState([0]);
   const activeNoticeIndex = activeNotices[0];
 
-  const NOTICE_LIST = [
+  const DUMMY_NOTICE_LIST = [
     {
       index: 0,
       title: '[시스템 점검] 사용불가 일자 및 시간 안내',
-      createdDate: '2024.01.16',
-      content:
+      createdTime: '2024.01.01',
+      message:
         '안녕하세요. 인생비서 팀입니다.\n\n아래 일자와 시간에 시스템 점검이 예정되어있으며, 이로 인해 한시적으로 서비스 사용이 불가할 예정입니다.\n\n여러분들께 보다 편리하고 안전한 서비스를 제공해 드리기 위해 노력하겠습니다. 감사합니다.\n\n일자 : 2024/03/01\n시간: AM 02:00 ~ 06:00',
     },
     {
       index: 1,
       title: '[업데이트] v1.0.2 업데이트 내용 안내',
-      createdDate: '2024.01.16',
-      content:
+      createdTime: '2024.02.01',
+      message:
         '안녕하세요. 인생비서 팀입니다.\n\n아래 일자와 시간에 시스템 점검이 예정되어있으며, 이로 인해 한시적으로 서비스 사용이 불가할 예정입니다.\n\n여러분들께 보다 편리하고 안전한 서비스를 제공해 드리기 위해 노력하겠습니다. 감사합니다.\n\n일자 : 2024/03/01\n시간: AM 02:00 ~ 06:00',
     },
     {
       index: 2,
       title: 'CS센터 운영 안내',
-      createdDate: '2024.01.16',
-      content:
+      createdTime: '2024.03.01',
+      message:
         '안녕하세요. 인생비서 팀입니다.\n\n아래 일자와 시간에 시스템 점검이 예정되어있으며, 이로 인해 한시적으로 서비스 사용이 불가할 예정입니다.\n\n여러분들께 보다 편리하고 안전한 서비스를 제공해 드리기 위해 노력하겠습니다. 감사합니다.\n\n일자 : 2024/03/01\n시간: AM 02:00 ~ 06:00',
     },
   ];
@@ -55,13 +59,15 @@ export function Notice(): React.JSX.Element {
           <AppIcon name="arrowDown" width={36} height={36} />
         )}
       </View>
-      <AppText style={styles.headerText}>{notice?.createdDate}</AppText>
+      <AppText style={styles.headerText}>
+        {notice?.createdTime || getFormattedDate(new Date(), '.')}
+      </AppText>
     </View>
   );
 
   const renderNoticeContent = (notice: object) => (
     <View style={styles.content}>
-      <AppText style={styles.contentText}>{notice?.content}</AppText>
+      <AppText style={styles.contentText}>{notice?.message}</AppText>
     </View>
   );
 
@@ -69,12 +75,26 @@ export function Notice(): React.JSX.Element {
     setActiveNotices(notices);
   };
 
+  useEffect(() => {
+    const fetchNoticeList = async () => {
+      const res = await fetchData('/notice', null);
+      const list = res.data.data;
+
+      if (res.status === 200) {
+        setNoticeList(list);
+      }
+    };
+
+    fetchNoticeList();
+  }, []);
+
   return (
     <View style={styles.container}>
-      {NOTICE_LIST.length > 0 ? (
+      {/* TODO: 실 데이터로 변경 */}
+      {DUMMY_NOTICE_LIST.length > 0 ? (
         <ScrollView>
           <Accordion
-            sections={NOTICE_LIST}
+            sections={DUMMY_NOTICE_LIST}
             activeSections={activeNotices}
             renderHeader={renderNoticeHeader}
             renderContent={renderNoticeContent}
