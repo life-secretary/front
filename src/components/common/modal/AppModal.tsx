67afe0ec9deb047f8d1df57d3a4a6df9d099a22d
ciocio97 <tmddusleo@icloud.com>
2024-05-19 14:36:04 +0900

    feat: add toast feature at ContentModal

--- src/components/common/modal/AppModal.tsx
+++ src/components/common/modal/AppModal.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import {StyleSheet, useWindowDimensions, Platform, View} from 'react-native';
 import Modal from 'react-native-modal';
+import AppIcon from '../AppIcon';
+import { AppText } from '../AppText';
+import Toast from 'react-native-toast-message';
 import type {ModalProps} from 'react-native-modal';
+import spacing from '@/styles/spacing';
+import color from '@/styles/color';
+import {font} from '@/styles/font';
+
 
 export type AppModalProps = Partial<ModalProps> & {
   /** [Required] Show the modal */
@@ -27,6 +34,23 @@ const AppModal = ({
   backdropOpacity = 1,
 }: AppModalProps): React.JSX.Element => {
   const {width, height} = useWindowDimensions();
+  const toastWidth = width - spacing.layoutPaddingHorizontal * 2;
+
+  const toastConfig = {
+    success: ({ props }: any) => (
+      <View style={[styles.toast, {width: toastWidth}]}>
+        <View style={styles.iconContainer}>
+          <AppIcon
+            name="checkBoxCircle"
+            width={24}
+            height={24}
+            styles={{fill: color.main.secondary, stroke: color.main.white}}
+          />
+        </View>
+        <AppText style={styles.toastText}>{props?.text}</AppText>
+      </View>
+    ),
+  };
 
   return (
     <Modal
@@ -40,6 +64,7 @@ const AppModal = ({
       onModalShow={() => {}} // Modal 이 완전히 띄워졌을 때 실행되는 callback 함수 (props로 전달)
       style={styles.modal}>
       <View style={styles.modalContainer}>{children}</View>
+      <Toast config={toastConfig} />
     </Modal>
   );
 };
@@ -59,6 +84,25 @@ const styles = StyleSheet.create({
         marginTop: 30,
       },
     }),
+  },
+  toast: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 14,
+    gap: 6,
+    borderRadius: 10,
+    backgroundColor: '#000E24B2',
+  },
+  toastText: {
+    fontSize: 14,
+    fontWeight: font.fontWeight.semiBold,
+    lineHeight: 16.71,
+    color: color.main.white,
+  },
+  iconContainer: {
+    width: 24,
+    height: 24,
   },
 });
 
