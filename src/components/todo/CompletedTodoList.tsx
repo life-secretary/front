f3162cee9e12aebeed146d4945fb4da628660ed1
Sol Ahn <sorry0194@gmail.com>
2024-05-28 02:50:03 +0900

    refactor: modify fetch todo list logic by using react-query

--- src/components/todo/CompletedTodoList.tsx
+++ src/components/todo/CompletedTodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {StyleSheet, View, FlatList} from 'react-native';
 import {AppText} from '@/components/common/AppText';
@@ -29,31 +29,38 @@ type Props = {
 };
 
 export function CompletedTodoList({isLoading}: Props): React.JSX.Element {
+  const [isEmpty, setIsEmpty] = useState(false);
   const totalTodoCount = useRecoilValue(todoListTotalCountState);
   const list = useRecoilValue(filteredTodoListState);
-  const isEmpty = totalTodoCount === 0;
+
+  useEffect(() => {
+    if (!isLoading && totalTodoCount === 0) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  }, [isLoading, totalTodoCount]);
 
   return (
     <View style={styles.container}>
-      {!isEmpty && isLoading && (
+      {isLoading && (
         <View style={styles.emptyListContainer}>
           <AppSpinner color={color.main.primary} />
         </View>
       )}
-      {!isLoading &&
-        (!isEmpty ? (
-          <>
-            <TodoCount title="완료 할 일" totalTodoCount={totalTodoCount} />
-            <FlatList
-              data={list}
-              renderItem={({item}) => <TodoCard item={item} />}
-              keyExtractor={(item: Todo) => String(item.id)}
-              contentContainerStyle={styles.listContainer}
-            />
-          </>
-        ) : (
-          <EmptyList />
-        ))}
+      {isEmpty ? (
+        <EmptyList />
+      ) : (
+        <>
+          <TodoCount title="완료 할 일" totalTodoCount={totalTodoCount} />
+          <FlatList
+            data={list}
+            renderItem={({item}) => <TodoCard item={item} />}
+            keyExtractor={(item: Todo) => String(item.id)}
+            contentContainerStyle={styles.listContainer}
+          />
+        </>
+      )}
     </View>
   );
 }
