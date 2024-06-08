51118492f00a5efd8db9148924328a1298ba3fa1
Sol Ahn <sorry0194@gmail.com>
2024-06-08 17:54:27 +0900

    fix: change notice list data

--- src/components/setting/notice/Notice.tsx
+++ src/components/setting/notice/Notice.tsx
@@ -27,35 +27,12 @@ export function Notice(): React.JSX.Element {
   const [activeNotices, setActiveNotices] = useState([0]);
   const activeNoticeIndex = activeNotices[0];
 
-  const DUMMY_NOTICE_LIST = [
-    {
-      index: 0,
-      title: '[시스템 점검] 사용불가 일자 및 시간 안내',
-      createdTime: '2024.01.01',
-      message:
-        '안녕하세요. 인생비서 팀입니다.\n\n아래 일자와 시간에 시스템 점검이 예정되어있으며, 이로 인해 한시적으로 서비스 사용이 불가할 예정입니다.\n\n여러분들께 보다 편리하고 안전한 서비스를 제공해 드리기 위해 노력하겠습니다. 감사합니다.\n\n일자 : 2024/03/01\n시간: AM 02:00 ~ 06:00',
-    },
-    {
-      index: 1,
-      title: '[업데이트] v1.0.2 업데이트 내용 안내',
-      createdTime: '2024.02.01',
-      message:
-        '안녕하세요. 인생비서 팀입니다.\n\n아래 일자와 시간에 시스템 점검이 예정되어있으며, 이로 인해 한시적으로 서비스 사용이 불가할 예정입니다.\n\n여러분들께 보다 편리하고 안전한 서비스를 제공해 드리기 위해 노력하겠습니다. 감사합니다.\n\n일자 : 2024/03/01\n시간: AM 02:00 ~ 06:00',
-    },
-    {
-      index: 2,
-      title: 'CS센터 운영 안내',
-      createdTime: '2024.03.01',
-      message:
-        '안녕하세요. 인생비서 팀입니다.\n\n아래 일자와 시간에 시스템 점검이 예정되어있으며, 이로 인해 한시적으로 서비스 사용이 불가할 예정입니다.\n\n여러분들께 보다 편리하고 안전한 서비스를 제공해 드리기 위해 노력하겠습니다. 감사합니다.\n\n일자 : 2024/03/01\n시간: AM 02:00 ~ 06:00',
-    },
-  ];
-
+  // TODO: notice 날짜 데이터 필요
   const renderNoticeHeader = (notice: object) => (
     <View style={styles.header}>
       <View style={styles.titleContainer}>
         <AppText style={styles.headerTitle}>{notice?.title}</AppText>
-        {notice?.index === activeNoticeIndex ? (
+        {noticeList.indexOf(notice) === activeNoticeIndex ? (
           <AppIcon name="arrowUp" width={36} height={36} />
         ) : (
           <AppIcon name="arrowDown" width={36} height={36} />
@@ -83,7 +60,12 @@ export function Notice(): React.JSX.Element {
       const list = res.data.data;
 
       if (res.status === 200) {
-        setNoticeList(list);
+        const orderedList = list.sort(
+          (a: string, b: string) =>
+            new Date(b.createdTime) - new Date(a.createdTime),
+        );
+
+        setNoticeList(orderedList);
       }
     };
 
@@ -92,11 +74,10 @@ export function Notice(): React.JSX.Element {
 
   return (
     <View style={styles.container}>
-      {/* TODO: 실 데이터로 변경 */}
-      {DUMMY_NOTICE_LIST.length > 0 ? (
+      {noticeList.length > 0 ? (
         <ScrollView>
           <Accordion
-            sections={DUMMY_NOTICE_LIST}
+            sections={noticeList}
             activeSections={activeNotices}
             renderHeader={renderNoticeHeader}
             renderContent={renderNoticeContent}
